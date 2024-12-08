637963bec513ab91d2867dcc6a062c1c005d0073
Deelolade <Oluwanisholahabeeb@icloud.com>
2024-12-08 13:37:08 +0100

    done with  state

--- src/App.jsx
+++ src/App.jsx
@@ -2,61 +2,47 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0)
-  const counter =()=>{
-    setCount(prevCount => prevCount + 1)
+  const counter = () => {
+    setCount(prev=> {
+      if (prev === 20){
+        alert(`counter is already set to ${count}`)
+      }
+      return prev + 1
+    })
   }
-  const Reduce =()=>{setCount(prevCount => prevCount >0 ? prevCount -1 : prevCount  )}
-  
+  const Reduce = () => { setCount(prevCount => prevCount > 0 ? prevCount - 1 : prevCount) }
   const [color, setColor] = useState('red');
-
-  // Function to change the color
   const changeColor = () => {
-    if (color ==="red") {
-      setColor('blue');
+    // setColor(prevColor => prevColor ==="red" ? "blue" : "red")
+    if (color === "red") {
+      setColor("blue")
+    }
+    else if (color === "blue") {
+      setColor("green")
     }
     else setColor("red")
   };
-  const [name, setName] =useState("Muhammed");
-
-  const nameChange=()=>{setName(name ==="Muhammed"? "Habeeb": "Muhammed")}
-
-
-
-  const [story, setStory] =useState({
-    name : "Muhammed",
-    age : 9,
-    school :"Premier Fountain school", 
-    grade: "Brilliance"
-  })
-  // const changeStory = () => {
-  //   setStory(story.name ==="Muhammed" ? "Muhammed" : "Habeeb")
-  // }
+  const [name, setName] = useState("Muhammed");
+  const nameChange = () => { setName(name === "Muhammed" ? "Habeeb" : "Muhammed") }
+  const [cover, setCover] = useState(false)
   return (
     <div>
       <button onClick={counter}>increase</button>
       <button>{count}</button>
       <button onClick={Reduce}>decrease</button>
-    
       <div>
-      <h1 style={{ backgroundColor: color, color:"white" }}>My favorite color is {color}</h1>
-      <button onClick={changeColor}>Change Color</button>
-    </div>
-    <h1>My name is {name}</h1>
-    <button onClick={nameChange}>change Name</button>
-
-    <h1>My story</h1>
-    <p>My name is {story.name}</p>
-    <p>I am {story.age} years old</p>
-    <p>I study at {story.school}</p>
-    <p>My Teacher recommends my {story.grade}</p>
-    <button onclick={changeStory}> change story</button>
-
-
-
+        <h1 style={{ backgroundColor: color, color: "white" }}>My favorite color is {color}</h1>
+        <button onClick={()=> setColor(prev => prev === "red"? "blue":"red")}>Change Color</button>
+      </div>
+      <h1>My name is {name}</h1>
+      <button onClick={nameChange}>change Name</button>
+      {cover && <h1 className='heading animated'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi.</h1>}
+      {cover ? <p>hello world </p> : <p>hello there</p>}
+      {cover ? <p>hello world</p> : null}
+      <button onClick={() => setCover(prev => !prev)}>{cover ? "Hide" : "Display"} Text</button>
     </div>
   )
 }
 
 
-// const Reduce =()=>{setCount (prevCount=> prevCount >0 ? prevCount -1 : prevCount)}
 export default App
