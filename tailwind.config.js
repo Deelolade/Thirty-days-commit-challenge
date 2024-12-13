886d9d937e33b0fd639e6a955c890b334dfc4657
Deelolade <Oluwanisholahabeeb@icloud.com>
2024-12-13 22:27:15 +0100

    Began using Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true, // All Tailwind classes will have higher specificity
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tell Tailwind to scan for these file types in your project
+  ],
+  theme: {
+    extend: {}, // Use this to customize the Tailwind theme (optional)
+  },
+  plugins: [],
+}
+
+
